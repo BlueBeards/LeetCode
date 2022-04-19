b9adbeac59b2395e6661109e2a8670ea6f5f43e7
lijinhui <you@example.com>
2022-04-19 15:34:40 +0800

    链表_合并两个有序链表

---
+++ LinkedList/src/com/bluebeard/_01_合并两个有序链表.java
@@ -0,0 +1,32 @@
+package com.bluebeard;
+
+public class _01_合并两个有序链表 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode headNode = new ListNode(-1);
+        ListNode prev = headNode;
+        while (list1 != null && list2 != null) {
+            int val1 = list1.val;
+            int val2 = list2.val;
+            if (val1 < val2) {
+                prev.next = list1;
+                list1 = list1.next;
+            } else {
+                prev.next = list2;
+                list2 = list2.next;
+            }
+            prev = prev.next;
+        }
+
+        prev.next = list1 == null ? list2 : list1;
+
+        return headNode.next;
+    }
+}
